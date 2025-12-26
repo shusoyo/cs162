41af5e40ecebecdb34b6b63864449dfbf875d7e6
shusoyo <190865826+shusoyo@users.noreply.github.com>
2025-12-26 05:17:38 +0000

    Finished my first CS 162 assignment

--- hw-intro/words/main.c
+++ hw-intro/words/main.c
@@ -21,16 +21,16 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
+#include "word_count.h"
+
 #include <assert.h>
+#include <ctype.h>
 #include <getopt.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
 #include <stdlib.h>
-
-#include "word_count.h"
+#include <string.h>
 
 /* Global data structure tracking the words encountered */
 WordCount *word_counts = NULL;
@@ -44,10 +44,14 @@ WordCount *word_counts = NULL;
  * Returns the total amount of words found in infile.
  * Useful functions: fgetc(), isalpha().
  */
-int num_words(FILE* infile) {
-  int num_words = 0;
+int num_words(FILE *infile) {
+    int num_words = 0;
+    WordCount *wc;
+    for (wc = word_counts; wc; wc = wc->next) {
+        num_words += wc->count;
+    }
 
-  return num_words;
+    return num_words;
 }
 
 /*
@@ -55,14 +59,56 @@ int num_words(FILE* infile) {
  *
  * Given infile, extracts and adds each word in the FILE to `wclist`.
  * Useful functions: fgetc(), isalpha(), tolower(), add_word().
- * 
+ *
  * As mentioned in the spec, your code should not panic or
  * segfault on errors. Thus, this function should return
  * 1 in the event of any errors (e.g. wclist or infile is NULL)
  * and 0 otherwise.
  */
 int count_words(WordCount **wclist, FILE *infile) {
-  return 0;
+    char buf[MAX_WORD_LEN], ch;
+
+    for (int i = 0; i < MAX_WORD_LEN; i++) {
+        buf[i] = '\0';
+    }
+
+    bool all_alpha = true;
+    int words_len = 0, i = 0;
+
+    while ((ch = fgetc(infile)) != EOF) {
+        if (!isalpha(ch)) {
+            buf[i] = ch;
+            if (words_len > 1 && all_alpha) {
+                // fprintf(stdout, "#debug: f:count_words %s\n", buf);
+                buf[i] = '\0';
+                char *word = (char *)malloc(sizeof(char) * (i + 2));
+                strcpy(word, buf);
+                if (add_word(wclist, word) == 1) {
+                    return 1;
+                }
+            }
+            all_alpha = true;
+            words_len = 0;
+            i = 0;
+            continue;
+        } else {
+            buf[i] = tolower(ch);
+            all_alpha = all_alpha && isalpha(buf[i]);
+            words_len += 1;
+        }
+        i++;
+    }
+
+    if (words_len > 1 && all_alpha) {
+        buf[i] = '\0';
+        char *word = (char *)malloc(sizeof(char) * (i + 2));
+        strcpy(word, buf);
+        if (add_word(wclist, word) == 1) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
 /*
@@ -70,82 +116,99 @@ int count_words(WordCount **wclist, FILE *infile) {
  * Useful function: strcmp().
  */
 static bool wordcount_less(const WordCount *wc1, const WordCount *wc2) {
-  return 0;
+    if (wc1->count != wc2->count) {
+        return wc1->count < wc2->count;
+    } else {
+        return strcmp(wc1->word, wc2->word) < 0;
+    }
 }
 
 // In trying times, displays a helpful message.
 static int display_help(void) {
-	printf("Flags:\n"
-	    "--count (-c): Count the total amount of words in the file, or STDIN if a file is not specified. This is default behavior if no flag is specified.\n"
-	    "--frequency (-f): Count the frequency of each word in the file, or STDIN if a file is not specified.\n"
-	    "--help (-h): Displays this help message.\n");
-	return 0;
+    printf(
+        "Flags:\n"
+        "--count (-c): Count the total amount of words in the file, or STDIN "
+        "if a file is not "
+        "specified. This is default behavior if no flag is specified.\n"
+        "--frequency (-f): Count the frequency of each word in the file, or "
+        "STDIN if a file is "
+        "not specified.\n"
+        "--help (-h): Displays this help message.\n");
+    return 0;
 }
 
 /*
  * Handle command line flags and arguments.
  */
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+    // Count Mode (default): outputs the total amount of words counted
+    bool count_mode = true;
+    int total_words = 0;
 
-  // Count Mode (default): outputs the total amount of words counted
-  bool count_mode = true;
-  int total_words = 0;
+    // Freq Mode: outputs the frequency of each word
+    bool freq_mode = false;
 
-  // Freq Mode: outputs the frequency of each word
-  bool freq_mode = false;
+    FILE *infile = NULL;
 
-  FILE *infile = NULL;
+    // Variables for command line argument parsing
+    int i;
+    static struct option long_options[] = {
+        { "count", no_argument, 0, 'c' },
+        { "frequency", no_argument, 0, 'f' },
+        { "help", no_argument, 0, 'h' },
+        { 0, 0, 0, 0 }
+    };
 
-  // Variables for command line argument parsing
-  int i;
-  static struct option long_options[] =
-  {
-      {"count", no_argument, 0, 'c'},
-      {"frequency", no_argument, 0, 'f'},
-      {"help", no_argument, 0, 'h'},
-      {0, 0, 0, 0}
-  };
+    // Sets flags
+    while ((i = getopt_long(argc, argv, "cfh", long_options, NULL)) != -1) {
+        switch (i) {
+            case 'c':
+                count_mode = true;
+                freq_mode = false;
+                break;
+            case 'f':
+                count_mode = false;
+                freq_mode = true;
+                break;
+            case 'h':
+                return display_help();
+        }
+    }
 
-  // Sets flags
-  while ((i = getopt_long(argc, argv, "cfh", long_options, NULL)) != -1) {
-      switch (i) {
-          case 'c':
-              count_mode = true;
-              freq_mode = false;
-              break;
-          case 'f':
-              count_mode = false;
-              freq_mode = true;
-              break;
-          case 'h':
-              return display_help();
-      }
-  }
+    if (!count_mode && !freq_mode) {
+        printf("Please specify a mode.\n");
+        return display_help();
+    }
 
-  if (!count_mode && !freq_mode) {
-    printf("Please specify a mode.\n");
-    return display_help();
-  }
+    /* Create the empty data structure */
+    init_words(&word_counts);
 
-  /* Create the empty data structure */
-  init_words(&word_counts);
+    if ((argc - optind) < 1) {
+        // No input file specified, instead, read from STDIN instead.
+        infile = stdin;
+        count_words(&word_counts, infile);
+        total_words += num_words(infile);
+    } else {
+        // At least one file specified. Useful functions: fopen(), fclose().
+        // The first file can be found at argv[optind]. The last file can be
+        // found at argv[argc-1].
+        for (int i = optind; i < argc; i++) {
+            if ((infile = fopen(argv[i], "r")) == NULL) {
+                exit(EXIT_FAILURE);
+            }
 
-  if ((argc - optind) < 1) {
-    // No input file specified, instead, read from STDIN instead.
-    infile = stdin;
-  } else {
-    // At least one file specified. Useful functions: fopen(), fclose().
-    // The first file can be found at argv[optind]. The last file can be
-    // found at argv[argc-1].
-  }
+            count_words(&word_counts, infile);
+            total_words += num_words(infile);
+            fclose(infile);
+        }
+    }
 
-  if (count_mode) {
-    printf("The total number of words is: %i\n", total_words);
-  } else {
-    wordcount_sort(&word_counts, wordcount_less);
-
-    printf("The frequencies of each word are: \n");
-    fprint_words(word_counts, stdout);
-}
-  return 0;
+    if (count_mode) {
+        printf("The total number of words is: %i\n", total_words);
+    } else {
+        wordcount_sort(&word_counts, wordcount_less);
+        printf("The frequencies of each word are: \n");
+        fprint_words(word_counts, stdout);
+    }
+    return 0;
 }
--- hw-intro/words/word_count.c
+++ hw-intro/words/word_count.c
@@ -25,49 +25,108 @@ Mutators take a reference to a list as first arg.
 /* Basic utilities */
 
 char *new_string(char *str) {
-  char *new_str = (char *) malloc(strlen(str) + 1);
-  if (new_str == NULL) {
-    return NULL;
-  }
-  return strcpy(new_str, str);
+    char *new_str = (char *)malloc(strlen(str) + 1);
+    if (new_str == NULL) {
+        return NULL;
+    }
+    return strcpy(new_str, str);
 }
 
 int init_words(WordCount **wclist) {
-  /* Initialize word count.
-     Returns 0 if no errors are encountered
-     in the body of this function; 1 otherwise.
-  */
-  *wclist = NULL;
-  return 0;
+    if (wclist == NULL) {
+        return 1;
+    }
+    *wclist = NULL;
+    return 0;
+}
+
+int init_word(WordCount **wclist) {
+    /* Initialize word count.
+       Returns 0 if no errors are encountered
+       in the body of this function; 1 otherwise.
+    */
+
+    WordCount *current = (WordCount *)malloc(sizeof(WordCount));
+    if (current == NULL) {
+        return 1;
+    }
+    current->count = 0;
+    current->word = NULL;
+    current->next = NULL;
+
+    *wclist = current;
+
+    return 0;
 }
 
 ssize_t len_words(WordCount *wchead) {
-  /* Return -1 if any errors are
-     encountered in the body of
-     this function.
-  */
+    /* Return -1 if any errors are
+       encountered in the body of
+       this function.
+    */
+    WordCount *wc = wchead;
     size_t len = 0;
+    while (wc != NULL) {
+        wc = wc->next;
+        len += 1;
+    }
     return len;
 }
 
 WordCount *find_word(WordCount *wchead, char *word) {
-  /* Return count for word, if it exists */
-  WordCount *wc = NULL;
-  return wc;
+    /* Return count for word, if it exists */
+    WordCount *wc = NULL;
+
+    while (wchead != NULL) {
+        bool eq = strlen(word) == strlen(wchead->word);
+        for (int i = 0; i < strlen(word) && eq; i++) {
+            if (word[i] != wchead->word[i]) {
+                eq = false;
+            }
+        }
+
+        // fprintf(stdout, "#debug: f:find_word %s\n", wchead->word);
+
+        if (eq) {
+            wc = wchead;
+            break;
+        }
+
+        wchead = wchead->next;
+    }
+
+    return wc;
 }
 
 int add_word(WordCount **wclist, char *word) {
-  /* If word is present in word_counts list, increment the count.
-     Otherwise insert with count 1.
-     Returns 0 if no errors are encountered in the body of this function; 1 otherwise.
-  */
- return 0;
+    /* If word is present in word_counts list, increment the count.
+       Otherwise insert with count 1.
+       Returns 0 if no errors are encountered in the body of this function; 1
+       otherwise.
+    */
+    WordCount *wc = find_word(*wclist, word);
+
+    if (wc == NULL) {
+        WordCount *current;
+        if (init_word(&current) == 0) {
+            current->word = word;
+            current->count = 1;
+            current->next = *wclist;
+            *wclist = current;
+        } else {
+            return 1;
+        }
+    } else {
+        wc->count += 1;
+    }
+
+    return 0;
 }
 
 void fprint_words(WordCount *wchead, FILE *ofile) {
-  /* print word counts to a file */
-  WordCount *wc;
-  for (wc = wchead; wc; wc = wc->next) {
-    fprintf(ofile, "%i\t%s\n", wc->count, wc->word);
-  }
+    /* print word counts to a file */
+    WordCount *wc;
+    for (wc = wchead; wc; wc = wc->next) {
+        fprintf(ofile, "%i\t%s\n", wc->count, wc->word);
+    }
 }
